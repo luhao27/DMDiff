0881bddb5bd10cc32f7672b501b52d5977ec7a98
luhao <54384903+luhao27@users.noreply.github.com>
2025-01-04 10:36:03 +0800

    Add files via upload

---
+++ utils/lh_eval.py
@@ -0,0 +1,156 @@
+from utils.evaluation import eval_atom_type, eval_bond_length
+import os
+import torch
+import numpy as np
+
+
+def print_dict(d, logger):
+    for k, v in d.items():
+        if v is not None:
+            logger.info(f'{k}:\t{v:.4f}')
+        else:
+            logger.info(f'{k}:\tNone')
+
+
+def print_ring_ratio(all_ring_sizes, logger):
+    ring_info = {}
+    for ring_size in range(3, 10):
+        n_mol = 0
+        for counter in all_ring_sizes:
+            if ring_size in counter:
+                n_mol += 1
+        logger.info(f'ring size: {ring_size} ratio: {n_mol / len(all_ring_sizes):.3f}')
+        ring_info[ring_size] = f'{n_mol / len(all_ring_sizes):.3f}'
+    return ring_info
+
+
+
+def eval_result(all_mol_stable, all_atom_stable, n_recon_success, n_eval_success, n_complete, num_samples, all_n_atom, all_bond_dist,
+                success_pair_dist, success_atom_types, results , result_path, logger, args, total=False): 
+     # 这里是count信息
+    logger.info(f'Evaluate done! {num_samples} samples in total.')
+    
+    # 分子稳定性，原子稳定性，重构成功率，评估成功率，完成率
+    fraction_mol_stable = all_mol_stable / num_samples
+    fraction_atm_stable = all_atom_stable / all_n_atom
+    fraction_recon = n_recon_success / num_samples
+    fraction_eval = n_eval_success / num_samples
+    fraction_complete = n_complete / num_samples
+    validity_dict = {
+        'mol_stable': fraction_mol_stable,
+        'atm_stable': fraction_atm_stable,
+        'recon_success': fraction_recon,
+        'eval_success': fraction_eval,
+        'complete': fraction_complete
+    }
+    print_dict(validity_dict, logger)
+    
+    # 各种化学键的指标
+    c_bond_length_profile = eval_bond_length.get_bond_length_profile(all_bond_dist)
+    c_bond_length_dict = eval_bond_length.eval_bond_length_profile(c_bond_length_profile)
+    logger.info('JS bond distances of complete mols: ')
+    print_dict(c_bond_length_dict, logger)
+
+    success_pair_length_profile = eval_bond_length.get_pair_length_profile(success_pair_dist)
+    success_js_metrics = eval_bond_length.eval_pair_length_profile(success_pair_length_profile)
+    print_dict(success_js_metrics, logger)
+
+    atom_type_js = eval_atom_type.eval_atom_type_distribution(success_atom_types)
+    logger.info('Atom type JS: %.4f' % atom_type_js)
+
+    if args.save:
+        eval_bond_length.plot_distance_hist(success_pair_length_profile,
+                                            metrics=success_js_metrics,
+                                            save_path=os.path.join(result_path, f'pair_dist_hist_{args.eval_step}.png'))
+    logger.info('Number of reconstructed mols: %d, complete mols: %d, evaluated mols: %d' % (
+        n_recon_success, n_complete, len(results)))
+    
+
+    # luhao add.
+    
+
+    
+
+
+
+    
+    
+    # QED, SA, Vina, ring_info
+    qed = [r['chem_results']['qed'] for r in results]
+    sa = [r['chem_results']['sa'] for r in results]
+    logger.info('QED:   Mean: %.3f Median: %.3f' % (np.mean(qed), np.median(qed)))
+    logger.info('SA:    Mean: %.3f Median: %.3f' % (np.mean(sa), np.median(sa)))
+    if args.docking_mode == 'qvina':
+        vina = [r['vina'][0]['affinity'] for r in results]
+        logger.info('Vina:  Mean: %.3f Median: %.3f' % (np.mean(vina), np.median(vina)))
+    elif args.docking_mode in ['vina_dock', 'vina_score']:
+        vina_score_only = [r['vina']['score_only'][0]['affinity'] for r in results]
+        vina_min = [r['vina']['minimize'][0]['affinity'] for r in results]
+        logger.info('Vina Score:  Mean: %.3f Median: %.3f' % (np.mean(vina_score_only), np.median(vina_score_only)))
+        logger.info('Vina Min  :  Mean: %.3f Median: %.3f' % (np.mean(vina_min), np.median(vina_min)))
+        if args.docking_mode == 'vina_dock':
+            vina_dock = [r['vina']['dock'][0]['affinity'] for r in results]
+            logger.info('Vina Dock :  Mean: %.3f Median: %.3f' % (np.mean(vina_dock), np.median(vina_dock)))
+
+    # check ring distribution
+    ring_info = print_ring_ratio([r['chem_results']['ring_size'] for r in results], logger)
+
+    if args.save:
+        torch.save({
+            'info': 'Number of reconstructed mols: %d, complete mols: %d, evaluated mols: %d' % (
+        n_recon_success, n_complete, len(results)),
+            'ring_info': ring_info,
+            'stability': validity_dict,
+            'c_bond_length_dict': c_bond_length_dict,
+            'success_js_metrics': success_js_metrics,
+            'atom_type_js': atom_type_js,
+            'bond_length': all_bond_dist,
+            'all_results': results
+        }, os.path.join(result_path, f'metrics_{args.eval_step}_wo_vina.pt'))
+
+
+
+
+
+
+
+
+"""
+def db = lmdb.open(
+            self.processed_path,
+            map_size=10*(1024*1024*1024),   # 10GB
+            create=True,
+            subdir=False,
+            readonly=False,  # Writable
+        )
+        with open(self.index_path, 'rb') as f:
+            index = pickle.load(f)
+
+        num_skipped = 0
+        with db.begin(write=True, buffers=True) as txn:
+            for i, (pocket_fn, ligand_fn, *_) in enumerate(tqdm(index)):
+                if pocket_fn is None: continue
+                try:
+                    data_prefix = self.raw_path
+                    pocket_dict = PDBProtein(os.path.join(data_prefix, pocket_fn)).to_dict_atom()
+                    ligand_dict = parse_sdf_file(os.path.join(data_prefix, ligand_fn))
+                    data = ProteinLigandData.from_protein_ligand_dicts(
+                        protein_dict=torchify_dict(pocket_dict),
+                        ligand_dict=torchify_dict(ligand_dict),
+                    )
+                    data.protein_filename = pocket_fn
+                    data.ligand_filename = ligand_fn
+                    data = data.to_dict()  # avoid torch_geometric version issue
+                    txn.put(
+                        key=str(i).encode(),
+                        value=pickle.dumps(data)
+                    )
+                except:
+                    num_skipped += 1
+                    print('Skipping (%d) %s' % (num_skipped, ligand_fn, ))
+                    continue
+        db.close()
+
+
+
+"""
